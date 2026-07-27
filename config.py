47f058f8c88b1b7cc30be83f6d8072678039d90f
kemabrr <kemabr.uc@gmail.com>
2026-07-28 02:14:08 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,11 @@ class Settings(BaseSettings):
     admin_sifre_hash: str = "admin123"
     cloudflare_worker_url: str = ""
     frontend_url: str = "https://sizin-sayt.pages.dev"
-
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 60 * 24  # 1 gün
+    access_token_expire_minutes: int = 60 * 24
 
     class Config:
         env_file = ".env"
-        extra = "ignore"  # Railway başga env variable-laryny ýykmasyn
+        extra = "ignore"
 
 settings = Settings()
