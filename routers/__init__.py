11fc162bed3322fbe828ee0d4bf4bf46e1d9a500
kemabrr <kemabr.uc@gmail.com>
2026-08-01 02:46:55 +0500

    Update __init__.py

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,5 +1,4 @@
 """Routers package"""
-"""Routers package"""
 from .auth_routes import router as auth_routes
 from .profile import router as profile
 from .payment import router as payment
@@ -7,4 +6,3 @@ from .teams import router as teams
 from .turnir import router as turnir
 from .admin import router as admin
 from .lobi import router as lobi
-
